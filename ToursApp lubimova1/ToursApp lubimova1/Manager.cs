4c6ba38ab519d99c575b429d02eaef78d80a85a3
lubimova08 <118671124+lubimova08@users.noreply.github.com>
2023-04-27 10:48:20 +0300

    Add files via upload

---
+++ ToursApp lubimova1/ToursApp lubimova1/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ToursApp_lubimova1
+{
+    internal class Manager
+    {
+        public static Frame MainFrame 
+        {
+            get; set;
+        }
+    }
+}
