b9e759519bedb837f8764907f9f3af7169134c82
lubimova08 <118671124+lubimova08@users.noreply.github.com>
2023-05-06 18:38:27 +0300

    Add files via upload

---
+++ RoomLibrary lub1/RoomLibrary lub1/Room.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomLibrary_lub1
+{
+    public class Room
+    {
+        double roomLengtc; // длина комнаты
+        double roomWidth;  //ширина комнаты
+        public double RoomLength
+        {
+            get { return RoomLength; }
+            set { RoomLength = value; }
+        }
+        /// <summary>
+        /// Мутод вычесления периметр комнаты
+        /// </summary>
+        /// <returns>Возвращает значение периметра</returns>
+        public double RoomPerimetr()
+        {
+            return 2 * (RoomLength + roomWidth);
+        }
+        /// <summary>
+        /// Мутод вычесления площадь комнаты
+        /// </summary>
+        /// <returns>Возвращает значение площади</returns>
+        public double RoomArea()
+        {
+            return RoomLength * roomWidth;
+        }
+        /// <summary>
+        /// Мутод вычесляет число квадратных метров на одного человека
+        /// </summary>
+        /// <param name="=np">Число человек</param>
+        /// <returns>Возвращает число квадратных метров</returns>
+        public double PersonArea(int np)
+        {
+            return RoomArea() / np;
+        }
+    }
+}
